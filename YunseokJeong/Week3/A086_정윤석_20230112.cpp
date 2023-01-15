f8ebffdb06e3affdbc05b15ffcf08256ed077d36
YS-Jeong-99 <besthcy9908@gmail.com>
2023-01-16 00:35:06 +0900

    Week3

--- YunseokJeong/Week3/A086_정윤석_20230112.cpp
+++ YunseokJeong/Week3/A086_정윤석_20230112.cpp
@@ -20,6 +20,7 @@ int main()
     {
         if (i >= 10)
         {
+<<<<<<< HEAD
             str = nums[i / 10] + " " + nums[i % 10];
             v.push_back({str, i});
         }
@@ -35,6 +36,23 @@ int main()
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i].second << " ";
+=======
+            s = nums[i / 10] + " " + nums[i % 10];
+            ans.push_back({s, i});
+        }
+        else
+        {
+            s = nums[i % 10];
+            ans.push_back({s, i});
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i].second << " ";
+>>>>>>> f04cd04 (Week3)
         if (i % 10 == 9)
             cout << '\n';
     }
